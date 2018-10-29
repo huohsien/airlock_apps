701518456c6f78f8cf41fbb917ab393f4cebb0bf
Victor Jiang <huohsien@me.com>
2018-10-29 15:31:12 +0800

    use result of BarCodeGetDecodersStatus to get the names of all symbologies and get propterties of each symbology using the API function BarCodeGetSymbology

--- js/testFunctions.js
+++ js/testFunctions.js
@@ -28,28 +28,48 @@ function handleAirlockReady() {
 
         decoderStatusPromise.then((decoders) => {
             var decoderProperties = Object.keys(decoders); // if comment it out, then will test decoders against predefined propterty in testData.js file
-            
+
             decoderProperties.forEach(propertyName => {
                 var decoderName = propertyName.replace(/^enable/, '');
                 decoderNames.push(decoderName);
-
                 result = `${result}\n <p>${propertyName}</p> <p>${decoders[propertyName]}</p>`;
             });
-            alert(decoderNames[0] + " was pushed")
 
             document.getElementById("result").innerHTML = result;
 
+            //Test: BarCodeGetSymbology(...)
+            // get symbology names from decoders status
+            try {
+                var symbologyNames = decoderNames;
+
+                symbologyNames.forEach((symbologyName) => {
+
+                    if (symbologyName != "_decoderMap") {
+
+                        var symbology = BarCodeGetSymbology(symbologyName);
+                        var symbologyProperties = Object.keys(symbology);
+
+                        symbologyProperties.forEach(propertyName => {
+                            result3 = `${result3}\n <p>${propertyName}</p> <p>${symbology[propertyName]}</p> <hr>`;
+                        });
+
+                    }
+                });
+
+                document.getElementById("result3").innerHTML = result3;
+
+            } catch (error) {
+                document.getElementById("result3").textContent = `error: ${error}`;
+            }
         });
 
     } catch (error) {
         document.getElementById("result").textContent = `error: ${error}`;
     }
 
-    Test: BarCodeGetReaderOutputConfiguration()
+    //Test: BarCodeGetReaderOutputConfiguration()
     try {
         var readerOutputConfig = BarCodeGetReaderOutputConfiguration();
-        // document.getElementById("result2").textContent = JSON.stringify(readerOutputConfig);
-
         var readerOutputConfigProperties = Object.keys(readerOutputConfig); // if comment it out, then will test decoders against predefined propterty in testData.js file
 
         readerOutputConfigProperties.forEach(propertyName => {
@@ -61,38 +81,16 @@ function handleAirlockReady() {
         document.getElementById("result2").textContent = `error: ${error}`;
     }
 
-    //Test: BarCodeGetSymbology(...)
-    // get symbology names from decoders status
-    try {
-        var symbologyNames = decoderNames;
-        result3 = `${result3}\n <p>${symbologyNames[0]}</p><hr>`;
-
-        symbologyNames.forEach((symbologyName) => {
-            
-            var symbology = BarCodeGetSymbology(symbologyName);
-
-            var symbologyProperties = Object.keys(symbology);
-
-            symbologyProperties.forEach(propertyName => {
-                result3 = `${result3}\n <p>${propertyName}</p> <p>${symbology[propertyName]}</p> <hr>`;
-            });
-        });
-
-        document.getElementById("result3").innerHTML = result3;
-
-    } catch (error) {
-        document.getElementById("result3").textContent = `error: ${error}`;
-    }
 
     //Test: BarCodeGetErrorMsg(...)
-    // try {
-    //     var errorMessage = BarCodeGetErrorMsg();
-    //     result4 = `<h3>${errorMessage}</h3><hr>`;
-    //     document.getElementById("result4").innerHTML = result4;
+    try {
+        var errorMessage = BarCodeGetErrorMsg();
+        result4 = `${result4}\n <p>${errorMessage}</p><hr>`;
+        document.getElementById("result4").innerHTML = result4;
 
-    // } catch (error) {
-    //     document.getElementById("result4").textContent = `error: ${error}`;
-    // }
+    } catch (error) {
+        document.getElementById("result4").textContent = `error: ${error}`;
+    }
 
     // Test: BarCodeGetUserPreferences(...)
     try {
